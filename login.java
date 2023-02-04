ae9052ae9f1261eadb9c2c942f225d1efd9f1d7c
Prajaktaghate <prajughate9@gmail.com>
2023-02-04 10:41:12 +0530

    file updated

---
+++ login.java
@@ -0,0 +1,10 @@
+class login {
+    void login1(){
+        System.out.println("login");
+        
+    }
+    public static void main(String args[]){
+        login l=new login();
+        l.login1();
+    }
+}
