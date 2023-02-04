1fd76b44aee82d42464bacb6017f665e79a76cd8
Prajaktaghate <prajughate9@gmail.com>
2023-02-04 10:49:58 +0530

    register added

---
+++ register.java
@@ -0,0 +1,10 @@
+class register {
+    void  register1(){
+        System.out.println(" register");
+        
+    }
+    public static void main(String args[]){
+         register l=new  register();
+        l. register1();
+    }
+}
